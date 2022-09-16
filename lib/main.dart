24a49cf4579494793423a2f8349211ddd10938cf
David Milai <davidmilai67@gmail.com>
2022-09-16 13:03:03 +0300

    print

--- lib/main.dart
+++ lib/main.dart
@@ -31,10 +31,32 @@ class _MyHomePageState extends State<MyHomePage> {
   int _counter = 0;
 
   MethodChannel _fmp10Channel = const MethodChannel('io_channel/fp');
+  static const platform = MethodChannel('samples.flutter.dev/battery');
 
   void _incrementCounter() {
+    print("Hey name is ${_fmp10Channel.name}");
+    print("Hey message is ${_fmp10Channel.binaryMessenger.toString()}");
+    print("Hey codec is ${_fmp10Channel.codec}");
+    print("Hey string is ${_fmp10Channel.toString()}");
+    _getBatteryLevel();
     setState(() {
       _counter++;
+    });
+  }
+
+  String _batteryLevel = 'Unknown battery level.';
+
+  Future<void> _getBatteryLevel() async {
+    String batteryLevel;
+    try {
+      final int result = await platform.invokeMethod('getBatteryLevel');
+      batteryLevel = 'Battery level at $result % .';
+    } on PlatformException catch (e) {
+      batteryLevel = "Failed to get battery level: '${e.message}'.";
+    }
+
+    setState(() {
+      _batteryLevel = batteryLevel;
     });
   }
 
@@ -48,19 +70,18 @@ class _MyHomePageState extends State<MyHomePage> {
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            Text(
-              'You have pushed the button many times:',
-            ),
+            Text('You have pushed the button many times:'),
             Text(
               '$_counter',
               style: Theme.of(context).textTheme.headline4,
             ),
+            Text(_batteryLevel),
             MaterialButton(
               onPressed: () async {
                 await _fmp10Channel.invokeMethod("printIt");
               },
               child: Text(
-                "PRINT IT",
+                "PRINT ",
                 style: TextStyle(
                   color: Colors.white,
                 ),
